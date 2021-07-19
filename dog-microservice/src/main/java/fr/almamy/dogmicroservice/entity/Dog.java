39173f8e972ce278400ec7e9ad8991c142d91c5d
Alm4my <thenixadm@gmail.com>
2021-07-19 20:29:05 +0000

    Add Getter to show the values in json response
    Entities need getters

--- dog-microservice/src/main/java/fr/almamy/dogmicroservice/entity/Dog.java
+++ dog-microservice/src/main/java/fr/almamy/dogmicroservice/entity/Dog.java
@@ -1,11 +1,13 @@
 package fr.almamy.dogmicroservice.entity;
 
+import lombok.Getter;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-@Entity
+@Entity @Getter
 public class Dog {
     //\\ Fields \\//
     @Id
