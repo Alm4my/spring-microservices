0c60c4952722e789f839a66e0a9b9378233c2001
Alm4my <thenixadm@gmail.com>
2021-07-19 20:00:00 +0000

    Add dummy data

---
+++ dog-microservice/src/main/resources/routines/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO
+    dog (id, name, breed, origin)
+VALUES
+       (1, 'Fluffy', 'Pomeranian', 'Mountain View, CA'),
+       (2, 'Spot', 'Pit Bull', 'Austin, TX'),
+       (3, 'Ginger', 'Cocker Spaniel', 'Kansas City, KS'),
+       (4, 'Lady', 'Direwolf', 'The North'),
+       (5, 'Sasha', 'Husky', 'Buffalo, NY');
+       (5, 'Henry', 'Baoule', 'Abidjan, CI');
